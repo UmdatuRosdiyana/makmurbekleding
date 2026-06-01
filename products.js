9c1de5bb3f26f3456a207e0c6e29cc463cf12f66
UmdatuRosdiyana <rosdiyanadiyana16@gmail.com>
2026-06-01 23:26:27 +0700

    Update product descriptions and add new items

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ nama:"Sarung Jok Avanza",
 kategori:"Sarung Jok",
 harga:"Rp1.500.000",
 gambar:"img/sarung-avanza.jpg",
-deskripsi:"Sarung jok custom premium untuk Toyota Avanza dengan bahan berkualitas dan jahitan rapi."
+deskripsi:"Sarung jok custom premium untuk Toyota Avanza dengan bahan berkualitas tinggi dan jahitan rapi."
 },
 
 {
@@ -24,7 +24,34 @@ nama:"Sarung Jok Brio",
 kategori:"Sarung Jok",
 harga:"Rp1.400.000",
 gambar:"img/sarung-brio.jpg",
-deskripsi:"Sarung jok custom premium untuk Honda Brio."
+deskripsi:"Sarung jok premium untuk Honda Brio."
+},
+
+{
+id:"sarung-innova",
+nama:"Sarung Jok Innova",
+kategori:"Sarung Jok",
+harga:"Rp1.800.000",
+gambar:"img/sarung-innova.jpg",
+deskripsi:"Sarung jok premium untuk Toyota Innova."
+},
+
+{
+id:"sarung-fortuner",
+nama:"Sarung Jok Fortuner",
+kategori:"Sarung Jok",
+harga:"Rp2.000.000",
+gambar:"img/sarung-fortuner.jpg",
+deskripsi:"Sarung jok premium untuk Toyota Fortuner."
+},
+
+{
+id:"sarung-pajero",
+nama:"Sarung Jok Pajero",
+kategori:"Sarung Jok",
+harga:"Rp2.000.000",
+gambar:"img/sarung-pajero.jpg",
+deskripsi:"Sarung jok premium untuk Mitsubishi Pajero."
 },
 
 {
@@ -33,7 +60,61 @@ nama:"Jok Paten Avanza",
 kategori:"Jok Paten",
 harga:"Rp3.500.000",
 gambar:"img/jokpaten-avanza.jpg",
-deskripsi:"Jok paten custom Avanza dengan desain premium dan kenyamanan maksimal."
+deskripsi:"Jok paten custom premium untuk Avanza."
+},
+
+{
+id:"jokpaten-xenia",
+nama:"Jok Paten Xenia",
+kategori:"Jok Paten",
+harga:"Rp3.500.000",
+gambar:"img/jokpaten-xenia.jpg",
+deskripsi:"Jok paten premium untuk Xenia."
+},
+
+{
+id:"jokpaten-brio",
+nama:"Jok Paten Brio",
+kategori:"Jok Paten",
+harga:"Rp3.200.000",
+gambar:"img/jokpaten-brio.jpg",
+deskripsi:"Jok paten premium untuk Honda Brio."
+},
+
+{
+id:"jokpaten-innova",
+nama:"Jok Paten Innova",
+kategori:"Jok Paten",
+harga:"Rp4.500.000",
+gambar:"img/jokpaten-innova.jpg",
+deskripsi:"Jok paten premium untuk Toyota Innova."
+},
+
+{
+id:"jokpaten-fortuner",
+nama:"Jok Paten Fortuner",
+kategori:"Jok Paten",
+harga:"Rp5.000.000",
+gambar:"img/jokpaten-fortuner.jpg",
+deskripsi:"Jok paten premium untuk Toyota Fortuner."
+},
+
+{
+id:"jokpaten-pajero",
+nama:"Jok Paten Pajero",
+kategori:"Jok Paten",
+harga:"Rp5.000.000",
+gambar:"img/jokpaten-pajero.jpg",
+deskripsi:"Jok paten premium untuk Mitsubishi Pajero."
+},
+
+{
+id:"jokpaten-bus",
+nama:"Jok Paten Bus",
+kategori:"Jok Paten",
+harga:"Rp12.000.000",
+gambar:"img/jokpaten-bus.jpg",
+deskripsi:"Jok paten premium untuk Bus Pariwisata dan AKAP."
 }
 
 ];
