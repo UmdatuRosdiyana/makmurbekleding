daaaff1aeb8f7ad71d6201704adff286665f25e9
UmdatuRosdiyana <rosdiyanadiyana16@gmail.com>
2026-06-01 23:04:46 +0700

    Add product data for various car seat covers

---
+++ products.js
@@ -0,0 +1,39 @@
+const products = [
+
+{
+id:"sarung-avanza",
+nama:"Sarung Jok Avanza",
+kategori:"Sarung Jok",
+harga:"Rp1.500.000",
+gambar:"img/sarung-avanza.jpg",
+deskripsi:"Sarung jok custom premium untuk Toyota Avanza dengan bahan berkualitas dan jahitan rapi."
+},
+
+{
+id:"sarung-xenia",
+nama:"Sarung Jok Xenia",
+kategori:"Sarung Jok",
+harga:"Rp1.500.000",
+gambar:"img/sarung-xenia.jpg",
+deskripsi:"Sarung jok custom premium untuk Daihatsu Xenia."
+},
+
+{
+id:"sarung-brio",
+nama:"Sarung Jok Brio",
+kategori:"Sarung Jok",
+harga:"Rp1.400.000",
+gambar:"img/sarung-brio.jpg",
+deskripsi:"Sarung jok custom premium untuk Honda Brio."
+},
+
+{
+id:"jokpaten-avanza",
+nama:"Jok Paten Avanza",
+kategori:"Jok Paten",
+harga:"Rp3.500.000",
+gambar:"img/jokpaten-avanza.jpg",
+deskripsi:"Jok paten custom Avanza dengan desain premium dan kenyamanan maksimal."
+}
+
+];
